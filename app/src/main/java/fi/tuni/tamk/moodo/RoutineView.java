afc815ef7484f498917038caec3752cfd9361892
Sorellum <lassi.markkinen@hotmail.com>
2020-02-21 17:31:57 +0200

    add routine attribute and textview to display routine name

--- app/src/main/java/fi/tuni/tamk/moodo/RoutineView.java
+++ app/src/main/java/fi/tuni/tamk/moodo/RoutineView.java
@@ -3,13 +3,19 @@ package fi.tuni.tamk.moodo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class RoutineView extends AppCompatActivity {
+    private Routine routine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.routine_view);
+        TextView routineTitle = findViewById(R.id.routine_title);
+
+        routine = (Routine) getIntent().getSerializableExtra("routine");
+        routineTitle.setText(routine.getName());
     }
 
 }
