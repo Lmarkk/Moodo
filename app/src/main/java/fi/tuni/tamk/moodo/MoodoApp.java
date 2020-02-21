4e6a6b1eb091df6ff80d4431c8d08ec503e8420d
Sorellum <lassi.markkinen@hotmail.com>
2020-02-21 17:31:27 +0200

    set onclicklistener to listview that changes activity based on routine

--- app/src/main/java/fi/tuni/tamk/moodo/MoodoApp.java
+++ app/src/main/java/fi/tuni/tamk/moodo/MoodoApp.java
@@ -1,24 +1,39 @@
 package fi.tuni.tamk.moodo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MoodoApp extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.moodo_app);
-        ListView listView = findViewById(R.id.routine_list);
+
+        listView = findViewById(R.id.routine_list);
         ArrayList<Routine> list = new ArrayList<>();
         list.add(new Routine("Pese hampaat"));
         list.add(new Routine("Vie roskat"));
+
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, list);
         listView.setAdapter(adapter);
-        // listView.setOnItemClickListener((parent, view1, position, id) -> callback.itemSelected(list.get(position)));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Routine listItem = (Routine) listView.getItemAtPosition(position);
+                Intent intent = new Intent(getApplicationContext(), RoutineView.class);
+                intent.putExtra("routine", listItem);
+                startActivity(intent);
+            }
+        });
     }
 }
