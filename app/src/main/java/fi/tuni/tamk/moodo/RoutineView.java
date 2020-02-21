4b93a3700db041c90b05a029bcb1d1698dc06dbc
Sorellum <lassi.markkinen@hotmail.com>
2020-02-21 16:43:30 +0200

    move listview from routineview layout to moodoapp layout

--- app/src/main/java/fi/tuni/tamk/moodo/RoutineView.java
+++ app/src/main/java/fi/tuni/tamk/moodo/RoutineView.java
@@ -2,13 +2,14 @@ package fi.tuni.tamk.moodo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class RoutineView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(...);
+        setContentView(R.layout.routine_view);
     }
 
 }
