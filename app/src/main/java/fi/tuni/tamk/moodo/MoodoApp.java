42b56a0d3aa6b5be350c7c02b8076756329bc0ce
Sorellum <lassi.markkinen@hotmail.com>
2020-02-21 16:59:58 +0200

    move listview to moodoapp

--- app/src/main/java/fi/tuni/tamk/moodo/MoodoApp.java
+++ app/src/main/java/fi/tuni/tamk/moodo/MoodoApp.java
@@ -2,6 +2,10 @@ package fi.tuni.tamk.moodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MoodoApp extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MoodoApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.moodo_app);
+        ListView listView = findViewById(R.id.routine_list);
+        ArrayList<Routine> list = new ArrayList<>();
+        list.add(new Routine("Pese hampaat"));
+        list.add(new Routine("Vie roskat"));
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, list);
+        listView.setAdapter(adapter);
+        // listView.setOnItemClickListener((parent, view1, position, id) -> callback.itemSelected(list.get(position)));
     }
 }
