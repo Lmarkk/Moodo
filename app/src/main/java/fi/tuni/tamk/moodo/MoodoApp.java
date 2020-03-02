539a8a88bc3272acee32f512689c36e7bb5f8ca2
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-02 20:14:18 +0200

    Added some comments for clarification and changed scope of class variables.

--- app/src/main/java/fi/tuni/tamk/moodo/MoodoApp.java
+++ app/src/main/java/fi/tuni/tamk/moodo/MoodoApp.java
@@ -13,7 +13,8 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MoodoApp extends AppCompatActivity {
-    ListView listView;
+    private ListView listView;
+    private ArrayList<Routine> routineList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +22,38 @@ public class MoodoApp extends AppCompatActivity {
         Log.d("MoodoApp", "onCreate");
         setContentView(R.layout.moodo_app);
 
+        // Create view and add routines to list
         listView = findViewById(R.id.routine_list);
-        ArrayList<Routine> list = new ArrayList<>();
-        list.add(new Routine("Pese hampaat"));
-        list.add(new Routine("Vie roskat"));
+        routineList = new ArrayList<>();
+        // Add the default routines for all
+        addDefaultRoutines();
+        // Adds custom routines that user has made
+        // addCustomRoutines();
 
-        ArrayAdapter<Routine> adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+        // Add list of routines to the list view
+        ArrayAdapter<Routine> adapter = new ArrayAdapter<>(this, R.layout.list_item, routineList);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Routine listItem = (Routine) listView.getItemAtPosition(position);
                 Intent intent = new Intent(getApplicationContext(), RoutineView.class);
+                // Add clicked list routine to be displayed in RoutineView
                 intent.putExtra("routine", listItem);
                 Toast.makeText(getApplicationContext(), listItem.toString(), Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
         });
+    }
+
+    // Adds default routines for all (unless deleted if possible)
+    private void addDefaultRoutines() {
+        routineList.add(new Routine("Pese hampaat"));
+        routineList.add(new Routine("Vie roskat"));
+    }
+
+    // Adds custom routines for each user
+    private void addCustomRoutines() {
+
     }
 }
