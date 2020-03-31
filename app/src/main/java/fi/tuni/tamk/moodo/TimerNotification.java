8090fadddb4eb586dfc20bcf8a66516c5ac5c501
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-31 12:05:03 +0300

    Add TimerNotification class for notification channel.

--- app/src/main/java/fi/tuni/tamk/moodo/TimerNotification.java
+++ app/src/main/java/fi/tuni/tamk/moodo/TimerNotification.java
@@ -1,11 +1,28 @@
 package fi.tuni.tamk.moodo;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 public class TimerNotification extends Application {
+    public static final String CHANNEL_ID = "timerServiceChannel";
 
     @Override
     public void onCreate() {
         super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "timerServiceChannel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService((NotificationManager.class));
+            manager.createNotificationChannel(serviceChannel);
+        }
     }
 }
