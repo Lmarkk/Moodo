cac89a81f4d9d85b80c05f0eb97303960b75de89
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-31 12:07:05 +0300

    Add persistent notification when app is running.

--- app/src/main/java/fi/tuni/tamk/moodo/TimerService.java
+++ app/src/main/java/fi/tuni/tamk/moodo/TimerService.java
@@ -1,10 +1,15 @@
 package fi.tuni.tamk.moodo;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import static fi.tuni.tamk.moodo.TimerNotification.CHANNEL_ID;
 
 public class TimerService extends Service {
 
@@ -21,7 +26,18 @@ public class TimerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Intent notificationIntent = new Intent(this, RoutineView.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
 
+        Notification notification  = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Timer Service")
+                .setContentText("Test Text")
+                .setSmallIcon(R.drawable.moodo_launcher)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
         return START_STICKY;
     }
 
