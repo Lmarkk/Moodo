c6e603c07e9e2ca769c187fd29834802daf2454e
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-11 23:11:35 +0300

    Add routines and subroutines from json configuration files.

--- app/src/main/java/fi/tuni/tamk/moodo/Activity/MoodoApp.java
+++ app/src/main/java/fi/tuni/tamk/moodo/Activity/MoodoApp.java
@@ -7,6 +7,7 @@ import fi.tuni.tamk.moodo.R;
 import fi.tuni.tamk.moodo.Classes.Routine;
 import fi.tuni.tamk.moodo.Classes.SubRoutine;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,7 +15,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MoodoApp extends AppCompatActivity {
     private ListView listView;
@@ -26,21 +33,21 @@ public class MoodoApp extends AppCompatActivity {
         Log.d("MoodoApp", "onCreate");
         setContentView(R.layout.moodo_app);
 
-        // Create view and add routines to list
+        // Create view and addroutines to list
         listView = findViewById(R.id.routine_list);
         routineList = new ArrayList<>();
-        // Add the default routines for all
-        addDefaultRoutines();
-        addDefaultSubRoutines();
 
-        // Add list of routines to the list view
+        // Load routine data from .json file
+        loadRoutineData();
+
+        // Add list ofroutines to the list view
         ArrayAdapter<Routine> adapter = new ArrayAdapter<>(this, R.layout.list_item, routineList);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Routine listItem = (Routine) listView.getItemAtPosition(position);
-                Log.d("MoodoApp", "Subroutines: " + listItem.getSubRoutines().toString());
+                //Log.d("MoodoApp", "Subroutines: " + listItem.getSubRoutines().toString());
                 Intent intent = new Intent(getApplicationContext(), RoutineView.class);
                 // Add clicked list routine to be displayed in RoutineView
                 intent.putExtra("routine", listItem);
@@ -51,39 +58,42 @@ public class MoodoApp extends AppCompatActivity {
         Util.initializeBackgroundTransition(findViewById(R.id.root_view_moodo));
     }
 
-    // Adds default routines for all (unless deleted if possible)
-    private void addDefaultRoutines() {
-        routineList.add(new Routine(1,"Pese hampaat", 300));
-        routineList.add(new Routine(2,"Vie roskat", 300));
-    }
-
-    // Adds sub routines for default routines using routine id's
-    private void addDefaultSubRoutines() {
-        ArrayList<SubRoutine> subRoutines = new ArrayList<>();
-        for(Routine item : routineList) {
-            switch (item.getId()) {
-                case 1:
-                    subRoutines.add(new SubRoutine(1, "Ota hammasharja"));
-                    subRoutines.add(new SubRoutine(2, "Lisää hammastahna"));
-                    subRoutines.add(new SubRoutine(3, "Pese hampaita kolme minuuttia"));
-                    subRoutines.add(new SubRoutine(4, "Huuhtele suu ja hammasharja"));
-                    break;
-                case 2:
-                    subRoutines.add(new SubRoutine(1, "Sulje roskapussi"));
-                    subRoutines.add(new SubRoutine(2, "Nosta roskapussi pois säiliöstä"));
-                    subRoutines.add(new SubRoutine(3, "Vie roskat roskikseen"));
-                    subRoutines.add(new SubRoutine(4, "Laita uusi roskapussi tilalle"));
-            }
-            if(subRoutines.size() != 0) {
-                Log.d("MoodoApp", subRoutines.toString());
-                item.setSubRoutines(subRoutines);
-                subRoutines.clear();
-            }
-        }
-    }
-
     public void openSettings(View view) {
         Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
+    }
+
+    private void loadRoutineData() {
+        Resources res = getResources();
+        InputStream is = res.openRawResource(R.raw.routine_data);
+        Scanner scanner = new Scanner(is);
+        StringBuilder builder = new StringBuilder();
+        while(scanner.hasNextLine()) {
+            builder.append(scanner.nextLine());
+        }
+        parseJson(builder.toString());
+    }
+
+    private void parseJson(String s) {
+        ArrayList<SubRoutine> subRoutines = new ArrayList<>();
+        try {
+            JSONObject root = new JSONObject(s);
+            JSONArray routineArray = root.getJSONArray("routines");
+            for(int i = 0; i < routineArray.length(); i++) {
+                JSONObject cr = routineArray.getJSONObject((i));
+                System.out.println(routineArray.getJSONObject(i));
+                Routine tempRoutine = new Routine(cr.getInt("id"), cr.getString("name"), cr.getInt("time"));
+                routineList.add(tempRoutine);
+                JSONArray subroutineArray = cr.getJSONArray("subroutines");
+                for(int j = 0; j < subroutineArray.length(); j++) {
+                    JSONObject csr = subroutineArray.getJSONObject(j);
+                    subRoutines.add(new SubRoutine(cr.getInt("id"), csr.getString("description")));
+                }
+                tempRoutine.setSubRoutines(subRoutines);
+                subRoutines.clear();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
