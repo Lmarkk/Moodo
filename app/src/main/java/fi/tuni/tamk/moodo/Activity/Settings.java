2cb1be54a856b6373953afab4142de294847a762
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-11 18:46:23 +0300

    Remove unnecessary code from receiving drawables from resources.

--- app/src/main/java/fi/tuni/tamk/moodo/Activity/Settings.java
+++ app/src/main/java/fi/tuni/tamk/moodo/Activity/Settings.java
@@ -21,15 +21,15 @@ public class Settings extends AppCompatActivity {
     private void initializeBackgroundTransition() {
         View containerView = findViewById(R.id.root_view_settings);
 
-        Drawable bg_0 = getResources().getDrawable(R.drawable.bg_blue);
-        Drawable bg_1 = getResources().getDrawable(R.drawable.bg_blueorange);
-        Drawable bg_2 = getResources().getDrawable(R.drawable.bg_greenyellow);
-        Drawable bg_3 = getResources().getDrawable(R.drawable.bg_green);
-        Drawable bg_4 = getResources().getDrawable(R.drawable.bg_orange);
-        Drawable bg_5 = getResources().getDrawable(R.drawable.bg_redorange);
-        Drawable bg_6 = getResources().getDrawable(R.drawable.bg_lightred);
-        Drawable bg_7 = getResources().getDrawable(R.drawable.bg_red);
-        Drawable bg_8 = getResources().getDrawable(R.drawable.bg_purple);
+        Drawable bg_0 = getDrawable(R.drawable.bg_blue);
+        Drawable bg_1 = getDrawable(R.drawable.bg_blueorange);
+        Drawable bg_2 = getDrawable(R.drawable.bg_greenyellow);
+        Drawable bg_3 = getDrawable(R.drawable.bg_green);
+        Drawable bg_4 = getDrawable(R.drawable.bg_orange);
+        Drawable bg_5 = getDrawable(R.drawable.bg_redorange);
+        Drawable bg_6 = getDrawable(R.drawable.bg_lightred);
+        Drawable bg_7 = getDrawable(R.drawable.bg_red);
+        Drawable bg_8 = getDrawable(R.drawable.bg_purple);
         Drawable[] drawables = {bg_0, bg_1, bg_2, bg_3, bg_4, bg_5, bg_6, bg_7, bg_8};
 
         CyclicTransitionDrawable ctd = new CyclicTransitionDrawable(drawables);
