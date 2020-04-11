3f55ce4daef69084754a329c0f78bcbfc9ea6525
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-11 18:33:15 +0300

    Add license and copyright information.

--- app/src/main/java/fi/tuni/tamk/moodo/Classes/CircleTimerView.java
+++ app/src/main/java/fi/tuni/tamk/moodo/Classes/CircleTimerView.java
@@ -1,3 +1,24 @@
+/**
+ * NOTICE
+ * This class uses source code from Circle Timer View project
+ * https://github.com/jiahuanyu/CircleTimerView, copyright Jiahuan 2015,
+ * licensed under the Apache 2.0 license.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Source https://github.com/jiahuanyu/CircleTimerView/blob/master/library/src/main/java/com/github/jiahuanyu/circletimerview/CircleTimerView.java
+ *
+ */
 package fi.tuni.tamk.moodo.Classes;
 
 import android.annotation.SuppressLint;
