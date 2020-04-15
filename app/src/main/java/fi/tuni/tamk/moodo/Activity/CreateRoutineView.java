1cbcc0dae33d8abe261d4cbab6be674b55a132d6
Sorellum <lassi.markkinen@hotmail.com>
2020-04-15 13:58:20 +0300

    Add CreateRoutineView Activity.

---
+++ app/src/main/java/fi/tuni/tamk/moodo/Activity/CreateRoutineView.java
@@ -0,0 +1,13 @@
+package fi.tuni.tamk.moodo.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateRoutineView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
