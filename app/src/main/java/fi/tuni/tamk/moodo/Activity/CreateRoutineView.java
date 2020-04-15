c7257a3b0dc235bfba7e6d943a15e0923007349e
Sorellum <lassi.markkinen@hotmail.com>
2020-04-15 15:15:16 +0300

    Instantiate ListView, ArrayList and Adapter for dynamic list in Activity.

--- app/src/main/java/fi/tuni/tamk/moodo/Activity/CreateRoutineView.java
+++ app/src/main/java/fi/tuni/tamk/moodo/Activity/CreateRoutineView.java
@@ -1,13 +1,29 @@
 package fi.tuni.tamk.moodo.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import fi.tuni.tamk.moodo.R;
 
 public class CreateRoutineView extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+    ArrayList<String> listItems = new ArrayList<String>();
+    ListView listView = findViewById(R.id.create_routine_listview);
+    ArrayAdapter<String> adapter;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_routine_view);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        listView.setAdapter(adapter);
     }
 }
