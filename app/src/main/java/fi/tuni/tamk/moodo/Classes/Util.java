96cd7dadd4b550715b8533e14873fc50bf5b41e7
Sorellum <lassi.markkinen@hotmail.com>
2020-05-25 17:12:36 +0300

    Add app startup information check methods.

--- app/src/main/java/fi/tuni/tamk/moodo/Classes/Util.java
+++ app/src/main/java/fi/tuni/tamk/moodo/Classes/Util.java
@@ -195,4 +195,37 @@ public class Util {
         reader.endObject();
         return new SubRoutine(id, description);
     }
+
+    public static AppStart checkAppStart(Context context) {
+        PackageInfo pInfo;
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        AppStart appStart = AppStart.NORMAL;
+        try {
+            pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            int lastVersionCode = sharedPreferences
+                    .getInt(LAST_APP_VERSION, -1);
+            int currentVersionCode = pInfo.versionCode;
+            appStart = checkAppStart(currentVersionCode, lastVersionCode);
+            // Update version in preferences
+            sharedPreferences.edit()
+                    .putInt(LAST_APP_VERSION, currentVersionCode).apply();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return appStart;
+    }
+
+    public static AppStart checkAppStart(int currentVersionCode, int lastVersionCode) {
+        if (lastVersionCode == -1) {
+            return AppStart.FIRST_TIME;
+        } else if (lastVersionCode < currentVersionCode) {
+            return AppStart.FIRST_TIME_VERSION;
+        } else if (lastVersionCode > currentVersionCode) {
+            return AppStart.NORMAL;
+        } else {
+            return AppStart.NORMAL;
+        }
+    }
 }
