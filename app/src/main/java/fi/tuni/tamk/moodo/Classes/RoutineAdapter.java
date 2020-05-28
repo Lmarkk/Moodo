3d0cbfa637f4b63db9cb158d17656c5b5ecded68
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-28 15:33:47 +0300

    Add edit button to subroutine list item, change color and icon of delete button.

--- app/src/main/java/fi/tuni/tamk/moodo/Classes/RoutineAdapter.java
+++ app/src/main/java/fi/tuni/tamk/moodo/Classes/RoutineAdapter.java
@@ -1,7 +1,6 @@
 package fi.tuni.tamk.moodo.Classes;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
