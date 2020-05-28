efec21e546d47abbe69ea9b0aae14a954eb69889
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-28 11:05:52 +0300

    Create class for custom adapter.

---
+++ app/src/main/java/fi/tuni/tamk/moodo/Classes/RoutineAdapter.java
@@ -0,0 +1,49 @@
+package fi.tuni.tamk.moodo.Classes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fi.tuni.tamk.moodo.R;
+
+public class RoutineAdapter extends ArrayAdapter<String> {
+
+    private List<String> list;
+    private Context context;
+    private TextView subRoutineDesc,
+            deleteSubRoutine;
+
+    public RoutineAdapter(Context context, List<String> mySubRoutines) {
+        super(context, 0, mySubRoutines);
+        this.list = mySubRoutines;
+        this.context = context;
+    }
+
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.adapter_view_layout, parent, false
+            );
+        }
+        final String currentSubRoutine = getItem(position);
+
+        subRoutineDesc = (TextView) listItemView.findViewById(R.id.adapterText1);
+        deleteSubRoutine = (TextView) listItemView.findViewById(R.id.adapterImageBtn1);
+
+        if (currentSubRoutine != null) {
+            subRoutineDesc.setText(currentSubRoutine);
+        }
+
+        deleteSubRoutine.setOnClickListener((v) -> {
+            list.remove(position);
+            notifyDataSetChanged();
+        });
+        return listItemView;
+    }
+}
